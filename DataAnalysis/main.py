ffc0f832ae4d880e17687b52d6f159a19009c31d
Amanda Velloso <32628647+vellsant@users.noreply.github.com>
2025-08-15 23:37:52 -0300

    Update main.py

--- DataAnalysis/main.py
+++ DataAnalysis/main.py
@@ -8,7 +8,7 @@ from prophet import plot
 
 st.title("Previsão do preço do petróleo tipo Brent em USD$")
 st.image('DataAnalysis/Brent-Crude-Oil.jpg')
-df = pd.read_csv('/Dados_IPEA.csv')
+df = pd.read_csv('DataAnalysis/Dados_IPEA.csv')
 df = df.dropna()
 df['Data'] = pd.to_datetime(df['Data'], dayfirst=True)
 df['Preco'] = df['Preco'].str.replace(',', '.')
@@ -44,6 +44,7 @@ df_futuro = pred[pred['ds'].dt.year == ano_pred]
 #st.write(pred.tail())
 
 st.line_chart(data=df_futuro, x='ds', y='yhat', x_label='Anos', y_label='Preço')
+
 
 
 
